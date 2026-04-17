1f890c995ba1c95e18bac330862bb843848d466d
Netocarvalhopb <netocarvalho@hotmail.com>
2026-04-17 20:15:44 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,12 @@
+public class Produto {
+    String nome;
+    double preco;
+    int estoque;
+
+    // construtor simples
+    public Produto(String n, double p, int e) {
+        nome = n;
+        preco = p;
+        estoque = e;
+    }
+}
